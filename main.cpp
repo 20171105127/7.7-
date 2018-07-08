6d62460d75882a4f20dc43928d6249ade1ceaa65
刘季和 <863824147@qq.com>
2018-07-08 11:22:10 +0800

    7.8
    存在换行问题

--- main.cpp
+++ main.cpp
@@ -1,32 +1,29 @@
 #include <cstdlib>
 #include<stdio.h>
-
 struct student
 {
-    
     int numbers;    //编号
     char name[10];    //名字
     char sex[10];     //性别
     int dateofbirt;   //出生日期
     char class1[15];   //班级
     char phoneNo[15];   //电话号
-    int judge1;    //裁判
+    int judge1;
     int judge2;
     int judge3;
     int judge4;
-    int judge5;
+    int judge5;   //裁判
     int score;    //成绩
 };
 int main()
 {
-    struct student s[100];
-    FILE *fp1,*fp2;
+    struct student s[100];               //定义结构数组
+    FILE *fp1,*fp2;                 //俩个文件指针
     int i=0,j;
-    
     fp1 =fopen("/Users/S20171105127/Desktop/student1.csv","r");
     if(fp1==NULL)
     {
-        printf("error\n");
+        printf("找不到文件\n");
         exit(-1);
     }
     else
@@ -34,31 +31,43 @@ int main()
         fscanf(fp1,"%*[^\n]%*c");
         while(!feof(fp1))
         {
-            fscanf(fp1,"%d,%[^,],%[^,],%d,%[^,],%[^,],%d,%d,%d,%d,%d",
-                   &s[i].numbers,&s[i].name,&s[i].sex,&s[i].dateofbirt,&s[i].class1,
-                   &s[i].phoneNo,&s[i].judge1,&s[i].judge2,&s[i].judge3,&s[i].judge4,&s[i].judge5);
+            fscanf(fp1,"%d",&s[i].numbers);
+            fscanf(fp1,"%[^,]",&s[i].name);
+            fscanf(fp1,"%[^,]",&s[i].sex);
+            fscanf(fp1,"%d",&s[i].dateofbirt);
+            fscanf(fp1,"%[^,]",&s[i].class1);
+            fscanf(fp1,"%[^,]",&s[i].phoneNo);
+            fscanf(fp1,"%d",&s[i].judge1);
+            fscanf(fp1,"%d",&s[i].judge2);
+            fscanf(fp1,"%d",&s[i].judge3);
+            fscanf(fp1,"%d",&s[i].judge4);
+            fscanf(fp1,"%d",&s[i].judge5);
             i++;
         }
         j=i;
-        
         for(i=0;i<j;i++)
         {
-            printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d\n",s[i].numbers,
-                   s[i].name,s[i].sex,s[i].dateofbirt,s[i].class1,s[i].phoneNo,
-                   s[i].judge1,s[i].judge2,s[i].judge3,s[i].judge4,s[i].judge5);
+            printf("%d",s[i].numbers);
+            printf("%s",s[i].name);
+            printf("%s",s[i].sex);
+            printf("%d",s[i].dateofbirt);
+            printf("%s",s[i].class1);
+            printf("%s",s[i].phoneNo);
+            printf("%d",s[i].judge1);
+            printf("%d",s[i].judge2);
+            printf("%d",s[i].judge3);
+            printf("%d",s[i].judge4);
+            printf("%d\n",s[i].judge5);
         }
-        
-        fclose(fp1);
+        fclose(fp1);              //关闭文件
     }
     j=i;
     int max[100],min[100];
-    
     for(i=0;i<j;i++)
     {
         max[i]=min[i]=s[i].judge1;
     }
     j=i;
-    
     for(i=0;i<j;i++)
     {
         if(s[i].judge2>max[i])
@@ -71,7 +80,6 @@ int main()
             max[i]=s[i].judge5;
     }
     j=i;
-    
     for(i=0;i<j;i++)
     {
         if(s[i].judge2<min[i])
@@ -82,11 +90,43 @@ int main()
             min[i]=s[i].judge4;
         if(s[i].judge5<min[i])
             min[i]=s[i].judge5;
-    }
+    }               //最值
     j=i;
-    
     for(i=0;i<j;i++)
     {
-        s[i].score=(s[i].judge1+s[i].judge2+s[i].judge3+s[i].judge4+s[i].judge5-max[i]-min[i])/3;
+        s[i].score=(s[i].judge1+s[i].judge2+s[i].judge3+s[i].judge4+s[i].judge5-max[i]-min[i])/3;       //平均值
     }
-    
+    j=i;
+    i=0;
+    fp2=fopen("/Users/S20171105127/Desktop/student2.csv","w");
+    fprintf(fp2,"numbers");
+    fprintf(fp2,"name");
+    fprintf(fp2,"sex");
+    fprintf(fp2,"dateofbirth");
+    fprintf(fp2,"class");
+    fprintf(fp2,"phoneNo");
+    fprintf(fp2,"judge1");
+    fprintf(fp2,"judge2");
+    fprintf(fp2,"judge3");
+    fprintf(fp2,"judge4");
+    fprintf(fp2,"judge5");
+    fprintf(fp2,"score\n");
+    while(i<j)
+    {
+        fprintf(fp2,"%d",s[i].numbers);
+        fprintf(fp2,"%s",s[i].name);
+        fprintf(fp2,"%s",s[i].sex);
+        fprintf(fp2,"%d",s[i].dateofbirt);
+        fprintf(fp2,"%s",s[i].class1);
+        fprintf(fp2,"%s",s[i].phoneNo);
+        fprintf(fp2,"%d",s[i].judge1);
+        fprintf(fp2,"%d",s[i].judge2);
+        fprintf(fp2,"%d",s[i].judge3);
+        fprintf(fp2,"%d",s[i].judge4);
+        fprintf(fp2,"%d",s[i].judge5);
+        fprintf(fp2,"%d",s[i].score);
+        i++;
+    }
+    fclose(fp2);               //关闭文件
+    return 0;
+}
